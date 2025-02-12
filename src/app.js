22ce504c9ac73a1f52fe0c804bed7a60030aa38e
Salomé Salas <154089468+lessalcu@users.noreply.github.com>
2025-02-12 18:35:06 -0500

    feat: :sparkles:

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const dotenv = require('dotenv');
+const cors = require('cors'); // Importar el paquete cors
+const app = express();
 const vehicleRoutes = require('./routes/vehicleRoutes');
+const dotenv = require('dotenv');
 
 dotenv.config();
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+// Habilitar CORS para todas las rutas y orígenes
+app.use(cors());
 
+// Usar express.json() para que el servidor pueda procesar JSON
 app.use(express.json());
 
 // Rutas
